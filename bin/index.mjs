7c4320182d6d198d40c2114c18b49eaa2fef3bbb
Wildhoney <adam.timberlake@gmail.com>
2019-02-04 22:11:59 +0000

    Setup initial Puppeteer

--- bin/index.mjs
+++ bin/index.mjs
@@ -1,1 +1,12 @@
 #!/usr/bin/env node
+
+import puppeteer from 'puppeteer';
+
+async function main() {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://www.google.com/');
+    await browser.close();
+}
+
+main();
