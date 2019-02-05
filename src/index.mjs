cd0cfad7f677e19493c829693432d96564b94fd1
Wildhoney <adam.timberlake@gmail.com>
2019-02-05 22:45:29 +0000

    Reordered Puppeteer commands

--- src/index.mjs
+++ src/index.mjs
@@ -5,16 +5,15 @@ import * as utils from './utils';
 export default async function main({ url, headless, hooks, helpers }) {
     const browser = await puppeteer.launch({ headless, devtools: true });
     const page = await browser.newPage();
-
-    await hooks.create(page);
-    await page.goto(url);
+    utils.mockNatives(page);
 
     page.on('pageerror', error => {
         helpers.log('error', error.toString());
         return void browser.close();
     });
 
-    utils.mockNatives(page);
+    await hooks.create(page);
+    await page.goto(url);
 
     for (const _ of R.range(0, 5000)) {
         await utils.runBehaviour({ page, helpers });
