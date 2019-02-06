ee720fcbdbb974f621dbd0a0c1478ae0be6b74a4
Wildhoney <adam.timberlake@gmail.com>
2019-02-06 21:49:56 +0000

    Setting 'hasErrored' prop

--- src/index.mjs
+++ src/index.mjs
@@ -33,6 +33,7 @@ export default async function main({
     });
 
     page.on('pageerror', async error => {
+        hasErrored = true;
         helpers.log('error', error.toString());
         isNavigation && (await utils.awaitPage(timeout));
         await page.screenshot({
