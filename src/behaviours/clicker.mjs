e651f6b14c8ec92ae215ec33542c588364acd6f7
Wildhoney <adam.timberlake@gmail.com>
2019-02-07 23:00:20 +0000

    Added initial click indicators

--- src/behaviours/clicker.mjs
+++ src/behaviours/clicker.mjs
@@ -7,6 +7,25 @@ export default async function clicker({ page, helpers }) {
 
         const x = Math.round(Math.random() * height);
         const y = Math.round(Math.random() * width);
+
+        await page.evaluate(
+            ({ x, y }) => {
+                const node = document.createElement('div');
+                node.style.all = 'initial';
+                node.style.transform = `translate(${x}px, ${y}px)`;
+                node.style.width = '10px';
+                node.style.height = '10px';
+                node.style.borderRadius = '50%';
+                node.style.border = '5px solid red';
+                node.style.position = 'absolute';
+                node.style.zIndex = 10000;
+                node.style.top = 0;
+                node.style.left = 0;
+                document.body.appendChild(node);
+            },
+            { x, y }
+        );
+
         await page.mouse.click(x, y);
 
         return void helpers.log('clicker', `Clicked at ${x}, ${y}`);
